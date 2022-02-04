75cd34279c9cf2b06c307fd3a567f6cd4861cb9a
Paula Clemente <clementepaula@gmail.com>
2022-02-04 13:50:45 -0300

    Subida al repositorio

---
+++ src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html',
+})
+export class BuscadorComponent implements OnInit {
+  heroes: any[] = [];
+  termino: string;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _heroesService: HeroesService,
+    private router: Router
+  ) {
+    this.termino = '';
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      // console.log('termino');
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes(params['termino']);
+      console.log(this.heroes);
+    });
+  }
+
+  verHeroe(idx: number) {
+    this.router.navigate(['/heroe', idx]);
+  }
+}
